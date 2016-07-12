0e32a2c2fdda97038a91e499cbca5bb17616c40b
Leszek Grzanka <Leszek.Grzanka@ifj.edu.pl>
2016-07-12 17:43:15 +0200

    simple tests

--- tests/test_generatemc.py
+++ tests/test_generatemc.py
@@ -13,6 +13,13 @@ class TestFunMethod(unittest.TestCase):
     def test_input(self):
         self.assertRaises(SystemExit, generatemc.main, ["."])
 
+    def test_input2(self):
+        with self.assertRaises(SystemExit) as cm:
+            generatemc.main(["-j", "2", "-p", "100",
+                             "tests/res/sample_fluka.inp"])
+        self.assertEqual(cm.exception.code,0)
+
+
 
 if __name__ == '__main__':
     unittest.main()
