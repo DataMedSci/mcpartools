e5f2cdb060013384ef192eff5272c2d0a7266dcf
Leszek Grzanka <leszek.grzanka@gmail.com>
2016-08-17 17:02:43 +0200

    merging

--- tests/test_generatemc.py
+++ tests/test_generatemc.py
@@ -10,14 +10,9 @@ class TestFunMethod(unittest.TestCase):
     def test_version(self):
         self.assertRaises(SystemExit, generatemc.main, ["--version"])
 
-    def test_input_ok(self):
-        generatemc.main(["-j", "2", "-p", "100", "tests/res/sample_fluka.inp"])
-
-    def test_input2(self):
-        with self.assertRaises(SystemExit) as cm:
-            generatemc.main(["-j", "2", "-p", "100",
-                             "tests/res/sample_fluka.inp"])
-        self.assertEqual(cm.exception.code,0)
+    def test_input_exit_code(self):
+        exit_code = generatemc.main(["-j", "2", "-p", "100", "tests/res/sample_fluka.inp"])
+        self.assertEqual(exit_code, 0)
 
     def test_input_bad(self):
         self.assertRaises(SystemExit, generatemc.main, ["-j", "2"])
