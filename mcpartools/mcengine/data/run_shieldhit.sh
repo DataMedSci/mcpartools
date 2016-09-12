9af5d3d7b6d9bbbd9dbf50663e62429e1485c70c
Leszek Grzanka <leszek.grzanka@gmail.com>
2016-09-12 15:19:32 +0200

    shieldhit workspace was not generated because generator wasn't implemented this commit includes its implementation

---
+++ mcpartools/mcengine/data/run_shieldhit.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Exit immediately if a simple command exits with a non-zero status.
+set -e
+
+# location of SHIELDHIT binary file
+SHIELDHIT_BIN={shieldhit_bin:s}
+
+# working directory, output files will be saved here
+WORK_DIR={working_directory:s}
+
+# number of particles per job
+PARTICLE_NO={particle_no:d}
+
+# seed of RNG
+RNG_SEED={rnd_seed:d}
+
+# main SHIELDHIT12A input files
+BEAM_FILE={beam_file:s}
+GEO_FILE={geo_file:s}
+MAT_FILE={mat_file:s}
+DETECT_FILE={detect_file:s}
+
+# execute simulation
+$SHIELDHIT_BIN -b $BEAM_FILE -g $GEO_FILE -m $MAT_FILE -d $DETECT_FILE -n $PARTICLE_NO -N $RNG_SEED $WORK_DIR
--- mcpartools/mcengine/shieldhit.py
+++ mcpartools/mcengine/shieldhit.py
@@ -1,41 +1,79 @@
+import logging
 import os
+from pkg_resources import resource_string
 
 from mcpartools.mcengine.mcengine import Engine
+
+logger = logging.getLogger(__name__)
 
 
 class ShieldHit(Engine):
 
-    run_script = """#!/bin/bash
-shieldhit .... {:s}
-"""
+    default_run_script_path = os.path.join('data', 'run_shieldhit.sh')
+    collect_script = os.path.join('data', 'collect_shieldhit.sh')
+
+    def __init__(self, input_path, mc_run_script):
+        Engine.__init__(self, input_path, mc_run_script)
+
+        # user didn't provided path to input scripts, use default
+        if self.run_script_path is None:
+            tpl = resource_string(__name__, self.default_run_script_path)
+            self.run_script_content = tpl.decode('ascii')
+            logger.debug("Using default run script: " + self.default_run_script_path)
+        else:
+            tpl_fd = open(self.run_script_path, 'r')
+            self.run_script_content = tpl_fd.read()
+            tpl_fd.close()
+            logger.debug("Using user run script: " + self.run_script_path)
+
+        self.collect_script_content = resource_string(__name__, self.collect_script).decode('ascii')
+
+        self.particle_no = 1
+        self.rng_seed = 1
 
     @property
     def input_files(self):
         base = self.input_path
         # TODO add *.txt files with stopping power
         # TODO add *.ctx/.hed files with CT scans
-        files = ['beam.dat', 'geo.dat', 'mat.dat', 'detect.dat']
-        result = [os.path.join(base, f) for f in files]
+        files = ('beam.dat', 'geo.dat', 'mat.dat', 'detect.dat')
+        result = (os.path.join(base, f) for f in files)
         return result
 
     def randomize(self, new_seed, output_dir=None):
-        pass
+        self.rng_seed = new_seed
 
     def set_particle_no(self, particle_no):
-        "in memory"
-        pass
+        self.particle_no = particle_no
 
     def save_input(self, output_dir):
-        "nothing, we will use switches -b, -g etc"
-        pass
+        logger.info("input files are not modified, we will used shieldhit switches instead")
 
     def save_run_script(self, output_dir, job_id):
-        contents = self.run_script.format(output_dir)
+        beam_file, geo_file, mat_file, detect_file = self.input_files
+        contents = self.run_script_content.format(
+            shieldhit_bin='shieldhit',
+            working_directory=output_dir,
+            particle_no=self.particle_no,
+            rnd_seed=self.rng_seed,
+            beam_file=beam_file,
+            geo_file=geo_file,
+            mat_file=mat_file,
+            detect_file=detect_file
+        )
         out_file_name = "run.sh"
         out_file_path = os.path.join(output_dir, out_file_name)
         out_fd = open(out_file_path, 'w')
         out_fd.write(contents)
         out_fd.close()
+        os.chmod(out_file_path, 0o750)
 
     def write_collect_script(self, output_dir):
-        pass
+        output_dir_abs_path = os.path.abspath(output_dir)
+        contents = self.collect_script_content.format(output_dir=output_dir_abs_path)
+        out_file_name = "collect.sh"
+        out_file_path = os.path.join(output_dir, out_file_name)
+        out_fd = open(out_file_path, 'w')
+        out_fd.write(contents)
+        out_fd.close()
+        os.chmod(out_file_path, 0o750)
