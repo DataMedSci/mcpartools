22f20248a88ad77783fdfcfb69af202d6bf80d07
Aga Rudnicka <ant6ant@gmail.com>
2016-12-16 09:55:58 +0100

    To discuss version of get scheduler #41

--- mcpartools/scheduler/common.py
+++ mcpartools/scheduler/common.py
@@ -1,6 +1,8 @@
 import logging
+from subprocess import check_call, CalledProcessError
 
 from mcpartools.scheduler.slurm import Slurm
+from mcpartools.scheduler.torque import Torque
 
 logger = logging.getLogger(__name__)
 
@@ -11,5 +13,17 @@ class SchedulerDiscover:
 
     @classmethod
     def get_scheduler(cls, scheduler_options):
-        logger.debug("Discovered job scheduler SLURM")
-        return Slurm(scheduler_options)
+        try:
+            check_call(['srun', '--version'], shell=True)
+            check_call(['sinfo', '--help'], shell=True)
+            logger.debug("Discovered job scheduler SLURM")
+            return Slurm(scheduler_options)
+        except CalledProcessError as e:
+            print("Slurm not found: ", str(e))
+        try:
+            check_call(['qstat', '-B'])
+            logger.debug("Discovered job scheduler Torque")
+            return Torque(scheduler_options)
+        except CalledProcessError as e:
+            print("Torque not found: ", str(e))
+        raise SystemError("No known batch system found!")
