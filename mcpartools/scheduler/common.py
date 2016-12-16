09404ed2cfe1367401529a2da089a2b59087a5ec
Aga Rudnicka <ant6ant@gmail.com>
2016-12-16 10:08:43 +0100

    Minor improvement in get scheduler

--- mcpartools/scheduler/common.py
+++ mcpartools/scheduler/common.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from subprocess import check_call, CalledProcessError
 
 from mcpartools.scheduler.slurm import Slurm
@@ -13,17 +14,19 @@ class SchedulerDiscover:
 
     @classmethod
     def get_scheduler(cls, scheduler_options):
-        try:
-            check_call(['srun', '--version'], shell=True)
-            check_call(['sinfo', '--help'], shell=True)
-            logger.debug("Discovered job scheduler SLURM")
-            return Slurm(scheduler_options)
-        except CalledProcessError as e:
-            print("Slurm not found: ", str(e))
-        try:
-            check_call(['qstat', '-B'])
-            logger.debug("Discovered job scheduler Torque")
-            return Torque(scheduler_options)
-        except CalledProcessError as e:
-            print("Torque not found: ", str(e))
+        with open(os.devnull, 'w') as FNULL:
+            try:
+                check_call(['srun', '--version'], stdout=FNULL, shell=True)
+                check_call(['sinfo', '--help'], stdout=FNULL, shell=True)
+                logger.debug("Discovered job scheduler SLURM")
+                return Slurm(scheduler_options)
+            except CalledProcessError as e:
+                print("Slurm not found: ", str(e))
+            try:
+                check_call(['man qsub'], stdout=FNULL, shell=True)
+                check_call(['qstat', '-B'], stdout=FNULL, shell=True)
+                logger.debug("Discovered job scheduler Torque")
+                return Torque(scheduler_options)
+            except CalledProcessError as e:
+                print("Torque not found: ", str(e))
         raise SystemError("No known batch system found!")
