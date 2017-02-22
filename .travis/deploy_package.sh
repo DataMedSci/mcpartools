bc5b13a9a89371aa27b07b967a8b6d8020f49ece
Leszek Grzanka <grzanka@agh.edu.pl>
2017-02-22 18:43:58 +0100

    github deployer for pypi added

--- .travis/deploy_package.sh
+++ .travis/deploy_package.sh
@@ -8,40 +8,6 @@ set -o pipefail # Return value of a pipeline as the value of the last command to
                 # exit with a non-zero status, or zero if all commands in the
                 # pipeline exit successfully.
 
-PYPIREPO=$1
-
-write_pypirc() {
-PYPIRC=~/.pypirc
-
-if [ -e "${PYPIRC}" ]; then
-    rm ${PYPIRC}
-fi
-
-touch ${PYPIRC}
-cat <<pypirc >${PYPIRC}
-[distutils]
-index-servers =
-    pypi
-
-[pypi]
-repository: https://pypi.python.org/pypi
-username: ${PYPIUSER}
-password: ${PYPIPASS}
-
-pypirc
-
-if [ ! -e "${PYPIRC}" ]; then
-    echo "ERROR: Unable to write file ~/.pypirc"
-    exit 1
-fi
-}
-
-
-# write .pypirc file with pypi repository credentials
-set +x
-write_pypirc
-set -x
-
 pip install -U wheel twine
 
 # make bdist universal package
@@ -56,11 +22,6 @@ pip install dist/*whl
 # test if it works
 generatemc --version
 generatemc --help
-
-# upload only if tag present
-if [[ $TRAVIS_TAG != "" ]]; then
-    twine upload -r $PYPIREPO dist/*
-fi
 
 # make pyinstaller package
 pip install pyinstaller
