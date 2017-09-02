c3e11beaa2edb7e610634602729576adc827c700
Michal Szczerba <plgpanszczerba@login01.pro.cyfronet.pl>
2017-09-02 13:41:11 +0200

    added comments about whereabouts of a log file and also displaying info about logging to a file

--- mcpartools/scheduler/data/submit_slurm.sh
+++ mcpartools/scheduler/data/submit_slurm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-LOGFILE="$(dirname $0)/submit.log"
+# Log file submit.log will be created in the same directory submit.sh is located
+LOGFILE="$(cd $(dirname $0) && pwd)/submit.log"
 printf "Job ID: " > "$LOGFILE"
 
-sbatch {options_args:s} --array=1-{jobs_no:d} --output="{log_dir:s}/output_%j_%a.log" --error="{log_dir:s}/error_%j_%a.log" --parsable {script_path:s} | cut -d ";" -f 1 >> "$LOGFILE"
+sbatch {options_args:s} --array=1-{jobs_no:d} --output="{log_dir:s}/output_%j_%a.log" --error="{log_dir:s}/error_%j_%a.log" --parsable {script_path:s} | cut -d ";" -f 1 >> "$LOGFILE" && echo "Saving logs to $LOGFILE"
--- mcpartools/scheduler/data/submit_torque.sh
+++ mcpartools/scheduler/data/submit_torque.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-LOGFILE="$(dirname $0)/submit.log"
+# Log file submit.log will be created in the same directory submit.sh is located
+LOGFILE="$(cd $(dirname $0) && pwd)/submit.log"
 printf "Job ID: " > "$LOGFILE"
 
-qsub {options_args:s} -t 1-{jobs_no:d} -o {log_dir:s} -e {log_dir:s} -terse {script_path:s} >> "$LOGFILE"
+qsub {options_args:s} -t 1-{jobs_no:d} -o {log_dir:s} -e {log_dir:s} -terse {script_path:s} >> "$LOGFILE" && echo "Saving logs to $LOGFILE"
