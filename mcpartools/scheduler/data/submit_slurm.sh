ba844767913891637bb70d4857fc609e1a844ab9
Michal Szczerba <plgpanszczerba@login01.pro.cyfronet.pl>
2017-09-02 19:55:45 +0200

    added logging submission time

--- mcpartools/scheduler/data/submit_slurm.sh
+++ mcpartools/scheduler/data/submit_slurm.sh
@@ -2,6 +2,9 @@
 
 # Log file submit.log will be created in the same directory submit.sh is located
 LOGFILE="$(cd $(dirname $0) && pwd)/submit.log"
+echo "Saving logs to $LOGFILE"
 printf "Job ID: " > "$LOGFILE"
 
-sbatch {options_args:s} --array=1-{jobs_no:d} --output="{log_dir:s}/output_%j_%a.log" --error="{log_dir:s}/error_%j_%a.log" --parsable {script_path:s} | cut -d ";" -f 1 >> "$LOGFILE" && echo "Saving logs to $LOGFILE"
+sbatch {options_args:s} --array=1-{jobs_no:d} --output="{log_dir:s}/output_%j_%a.log" --error="{log_dir:s}/error_%j_%a.log" --parsable {script_path:s} | cut -d ";" -f 1 >> "$LOGFILE"
+
+printf "Submission time: `date +"%Y-%m-%d %H:%M:%S"`" >> "$LOGFILE"
--- mcpartools/scheduler/data/submit_torque.sh
+++ mcpartools/scheduler/data/submit_torque.sh
@@ -2,6 +2,9 @@
 
 # Log file submit.log will be created in the same directory submit.sh is located
 LOGFILE="$(cd $(dirname $0) && pwd)/submit.log"
+echo "Saving logs to $LOGFILE"
 printf "Job ID: " > "$LOGFILE"
 
-qsub {options_args:s} -t 1-{jobs_no:d} -o {log_dir:s} -e {log_dir:s} -terse {script_path:s} >> "$LOGFILE" && echo "Saving logs to $LOGFILE"
+qsub {options_args:s} -t 1-{jobs_no:d} -o {log_dir:s} -e {log_dir:s} -terse {script_path:s} >> "$LOGFILE"
+
+printf "Submission time: `date +"%Y-%m-%d %H:%M:%S"`" >> "$LOGFILE
