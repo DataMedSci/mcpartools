b9b6c5b830e789e0ad76063e0c54d16c97582b81
Michal Szczerba <plgpanszczerba@login01.pro.cyfronet.pl>
2017-09-03 10:32:23 +0200

    added logging stderr from batch system command

--- mcpartools/scheduler/data/submit_slurm.sh
+++ mcpartools/scheduler/data/submit_slurm.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
 # Log file submit.log will be created in the same directory submit.sh is located
+# submit.log is for storing stdout and stderr of sbatch command, for log info from individual jobs see {log_dir:s} directory
 LOGFILE="$(cd $(dirname $0) && pwd)/submit.log"
+echo -n "" > "$LOGFILE"
+
+OUT=`mktemp`
+ERR=`mktemp`
+sbatch {options_args:s} --array=1-{jobs_no:d} --output="{log_dir:s}/output_%j_%a.log" --error="{log_dir:s}/error_%j_%a.log" --parsable {script_path:s} > $OUT 2>$ERR
+
 echo "Saving logs to $LOGFILE"
-printf "Job ID: " > "$LOGFILE"
 
-sbatch {options_args:s} --array=1-{jobs_no:d} --output="{log_dir:s}/output_%j_%a.log" --error="{log_dir:s}/error_%j_%a.log" --parsable {script_path:s} | cut -d ";" -f 1 >> "$LOGFILE"
+# if sbatch command ended with a success log following info
+if [ $? -eq 0 ] ; then
+	echo "Job ID: `cat $OUT | cut -d ";" -f 1`" > "$LOGFILE"
+	echo "Submission time: `date +"%Y-%m-%d %H:%M:%S"`" >> "$LOGFILE"
+fi
 
-printf "Submission time: `date +"%Y-%m-%d %H:%M:%S"`" >> "$LOGFILE"
+# if output from stderr isn't an empty string then log it as well to submit.log
+if [ "`cat $ERR`" != "" ] ; then
+	echo "---------------------" >> "$LOGFILE"
+	echo "ERROR MESSAGE" >>"$LOGFILE"	
+	echo "---------------------" >> "$LOGFILE"
+	cat $ERR >> "$LOGFILE"
+fi
+
+rm $OUT
+rm $ERR
--- mcpartools/scheduler/data/submit_torque.sh
+++ mcpartools/scheduler/data/submit_torque.sh
@@ -1,10 +1,29 @@
 #!/usr/bin/env bash
 
 # Log file submit.log will be created in the same directory submit.sh is located
+# submit.log is for storing stdout and stderr of qsub command, for log info from individual jobs see {log_dir:s} directory
 LOGFILE="$(cd $(dirname $0) && pwd)/submit.log"
+echo -n "" > "$LOGFILE"
+
+OUT=`mktemp`
+ERR=`mktemp`
+qsub {options_args:s} -t 1-{jobs_no:d} -o {log_dir:s} -e {log_dir:s} -terse {script_path:s} > $OUT 2> $ERR
+
 echo "Saving logs to $LOGFILE"
-printf "Job ID: " > "$LOGFILE"
 
-qsub {options_args:s} -t 1-{jobs_no:d} -o {log_dir:s} -e {log_dir:s} -terse {script_path:s} >> "$LOGFILE"
+# if qsub command ended with a success log following info
+if [ $? -eq 0 ] ; then
+        echo "Job ID: `cat $OUT | cut -d ";" -f 1`" > "$LOGFILE"
+        echo "Submission time: `date +"%Y-%m-%d %H:%M:%S"`" >> "$LOGFILE"
+fi
 
-printf "Submission time: `date +"%Y-%m-%d %H:%M:%S"`" >> "$LOGFILE
+# if output from stderr isn't an empty string then log it as well to submit.log
+if [ "`cat $ERR`" != "" ] ; then
+        echo "---------------------" >> "$LOGFILE"
+        echo "ERROR MESSAGE" >>"$LOGFILE"
+        echo "---------------------" >> "$LOGFILE"
+        cat $ERR >> "$LOGFILE"
+fi
+
+rm $OUT
+rm $ERR
