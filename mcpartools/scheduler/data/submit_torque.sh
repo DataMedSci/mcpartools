252526d274411d2971c2613518d5d3a8fe4a00ab
Michal Szczerba <plgpanszczerba@login01.pro.cyfronet.pl>
2017-09-08 19:36:26 +0200

    added comments for clearer code understanding

--- mcpartools/scheduler/data/submit_torque.sh
+++ mcpartools/scheduler/data/submit_torque.sh
@@ -5,20 +5,23 @@
 LOGFILE="$(cd $(dirname $0) && pwd)/submit.log"
 echo -n "" > "$LOGFILE"
 
+# Create temporary files for parsing stdout and stderr output from qsub command before storing them in submit.log
 OUT=`mktemp`
 ERR=`mktemp`
+# On exit or if the script is interrupted (i.e. by receiving SIGINT signal) delete temporary files
 trap "rm -f $OUT $ERR" EXIT
+
 qsub {options_args:s} -t 1-{jobs_no:d} -o {log_dir:s} -e {log_dir:s} -terse {script_path:s} > $OUT 2> $ERR
 
 echo "Saving logs to $LOGFILE"
 
-# if qsub command ended with a success log following info
+# If qsub command ended with a success log following info
 if [ $? -eq 0 ] ; then
         echo "Job ID: `cat $OUT | cut -d ";" -f 1`" > "$LOGFILE"
         echo "Submission time: `date +"%Y-%m-%d %H:%M:%S"`" >> "$LOGFILE"
 fi
 
-# if output from stderr isn't an empty string then log it as well to submit.log
+# If output from stderr isn't an empty string then log it as well to submit.log
 if [ "`cat $ERR`" != "" ] ; then
         echo "---------------------" >> "$LOGFILE"
         echo "ERROR MESSAGE" >>"$LOGFILE"
